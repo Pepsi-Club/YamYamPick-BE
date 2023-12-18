368af2f325b93d3ecaaec6208e9b2555eb1ea6c9
HKLeeeee <dlapdlf1739@gmail.com>
2023-12-18 15:16:18 +0900

    initial commit

---
+++ src/main/java/pepsiclub/yamyampick/YamyampickApplication.java
@@ -0,0 +1,13 @@
+package pepsiclub.yamyampick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamyampickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamyampickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pepsiclub/yamyampick/YamyampickApplicationTests.java
@@ -0,0 +1,13 @@
+package pepsiclub.yamyampick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YamyampickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
